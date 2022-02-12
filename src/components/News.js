2ab41beff37d5a58bf4ba0c095325330c211f2ba
Sajjad Mazhar <sajjad21@navgurukul.org>
2022-02-12 11:00:36 +0530

    added dynamic title

--- src/components/News.js
+++ src/components/News.js
@@ -16,14 +16,19 @@ export class News extends Component {
         category:PropTypes.string
     }
     
-    constructor(){
-        super();
+    capitalize(str){
+        return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+
+    constructor(props){
+        super(props);
         console.log("cons from news")
         this.state = {
             articles:[],
             loading:false,
             page:1
         }
+        document.title = `${this.capitalize(this.props.category)} - NewsOnTop`;
     }
 
     async updateNews(){
@@ -41,15 +46,7 @@ export class News extends Component {
 
     // this function works as soon as the page loads
     async componentDidMount(){
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=34064a34625f4e8188a398ba7d829319&page=1&pagesize=6`;
-        this.setState({loading:true})
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({
-            page:1,
-            articles:parsedData.articles,
-            loading:false
-        })
+        this.updateNews()
     }
     handleNextClick = async ()=>{
         // try{
@@ -100,12 +97,12 @@ export class News extends Component {
         
         return (
             <div className='container my-3'>
-                <h2>Top News - {this.props.category}</h2>
+                <h2>Top News - Headlines on {this.capitalize(this.props.category)}</h2>
                 {this.state.loading && <Spinner/>}
                 <div className='row'>
                 {!this.state.loading && this.state.articles.map((elem)=>{
                     return <div className='col-md-4 my-4' key={elem.url}>
-                    <NewsItem badge={this.props.badge} title={elem.title?elem.title.slice(0, 45):""} description={elem.description?elem.description.slice(0,50):""} imgUrl={elem.urlToImage} newsUrl = {elem.url} author={elem.author} timeStamp={elem.publishedAt} source={elem.source.name}/>
+                    <NewsItem badge={this.props.badge} title={elem.title} description={elem.description?elem.description.slice(0,50)+'...':"..."} imgUrl={elem.urlToImage} newsUrl = {elem.url} author={elem.author} timeStamp={elem.publishedAt} source={elem.source.name}/>
                 </div>
                 })}  <div id="root"></div>
                 </div>
